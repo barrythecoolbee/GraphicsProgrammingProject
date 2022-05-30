a89763794ffbbfe6a3bd32911e7026f405e16a9b
Beatriz <beatrizsa1906@gmail.com>
2022-05-30 15:04:02 +0200

    Wave didn't work :c
    Setup water fountain

---
+++ ParticleSystems&Anim/WaveShader/main.cpp
@@ -0,0 +1,339 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
+
+#include "shader.h"
+#include "camera.h"
+#include "model.h"
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+// function declarations
+// ---------------------
+void drawObjects();
+void setLightUniforms();
+void drawGui();
+void drawQuad();
+
+// glfw and input functions
+// ------------------------
+void processInput(GLFWwindow* window);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void key_input_callback(GLFWwindow* window, int button, int other, int action, int mods);
+void cursor_input_callback(GLFWwindow* window, double posX, double posY);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+// screen settings
+// ---------------
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720;
+
+// global variables used for rendering
+// -----------------------------------
+Shader* shader;
+Shader* wave_shading;
+Model* floorModel;
+Camera camera(glm::vec3(0.0f, 1.6f, 5.0f));
+
+// global variables used for control
+// ---------------------------------
+float lastX = (float)SCR_WIDTH / 2.0;
+float lastY = (float)SCR_HEIGHT / 2.0;
+float deltaTime;
+bool isPaused = false; // stop camera movement when GUI is open
+
+
+
+
+
+// structure to hold lighting info
+// -------------------------------
+struct Config {
+
+    // ambient light
+    glm::vec3 ambientLightColor = { 1.0f, 1.0f, 1.0f };
+    glm::vec3 ambientLightIntensity = { 1.0f, 1.0f, 1.0f };
+	glm::vec4 ambientLightPosition = { -0.8f, 2.4f, 0.0f, 1.0f };
+
+    // material
+    glm::vec3 ambientReflectance = { 0.2f, 0.2f, 0.2f };
+    glm::vec3 diffuseReflectance = { 0.9f, 0.5f, 0.3f };
+    glm::vec3 specularReflectance = { 0.8f, 0.8f, 0.8f };
+    float specularExponent = 100.0f;
+
+} config;
+
+
+
+
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Exercise 5", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, cursor_input_callback);
+    glfwSetKeyCallback(window, key_input_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // load the shaders and the 3D models
+    // ----------------------------------
+    wave_shading = new Shader("shaders/wave.vert", "shaders/wave.frag");
+    shader = wave_shading;
+    floorModel = new Model("floor/floor.obj");
+
+    // set up the z-buffer
+    // -------------------
+    glDepthRange(-1, 1); // make the NDC a right handed coordinate system, with the camera pointing towards -z
+    glEnable(GL_DEPTH_TEST); // turn on z-buffer depth test
+    glDepthFunc(GL_LESS); // draws fragments that are closer to the screen in NDC
+
+
+    // Dear IMGUI init
+    // ---------------
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330 core");
+
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        static float lastFrame = 0.0f;
+        float currentFrame = (float)glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        processInput(window);
+
+        glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shader->use();
+        setLightUniforms();
+        drawObjects();
+
+        if (isPaused) {
+            drawGui();
+        }
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Cleanup
+    // -------
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+	
+    delete floorModel;
+    delete wave_shading;
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+
+
+
+
+
+void drawGui() {
+    // NEW! we are using the header library Dear ImGui to set the variables in a graphical interface
+    //  here is where we define the buttons and how they map to the variables in our program
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    {
+        ImGui::Begin("Settings");
+
+        ImGui::Text("Ambient light: ");
+        ImGui::ColorEdit3("ambient light color", (float*)&config.ambientLightColor);
+        ImGui::Separator();
+		
+        ImGui::Text("Material: ");
+        ImGui::SliderFloat("specular exponent", &config.specularExponent, 0.0f, 100.0f);
+        ImGui::Separator();
+
+        ImGui::Text("Shading model: ");
+        {
+            if (ImGui::RadioButton("Wave", shader == wave_shading)) { shader = wave_shading; }
+        }
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+    }
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void setLightUniforms() {
+    shader->setVec3("LightIntensity", config.ambientLightColor * config.ambientLightIntensity);
+    shader->setVec4("LightPosition", config.ambientLightPosition);
+}
+void drawObjects() {
+
+	// camera position
+    shader->setVec3("camPosition", camera.Position);
+    shader->setFloat("Time", deltaTime);
+    shader->setVec3("ambientReflectance", config.ambientReflectance);
+    shader->setVec3("diffuseReflectance", config.diffuseReflectance);
+    shader->setVec3("specularReflectance", config.specularReflectance);
+    shader->setFloat("specularExponent", config.specularExponent);
+
+    // camera parameters
+    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.3f, 100.0f);
+    glm::mat4 view = camera.GetViewMatrix();
+    glm::mat4 model = glm::mat4(1.0);
+	glm::mat4 mv = view * model;
+	glm::mat4 mvp = projection * view * model;
+
+	// draw the floor
+	model = glm::scale(model, glm::vec3(10.0f, 10.0f, 10.0f));
+	shader->setMat4("ModelViewMatrix", model);
+	//shader->setMat3("NormalMatrix", glm::mat3(glm::vec3(model[0]), glm::vec3(model[1]), glm::vec3(model[2])));
+	shader->setMat4("MVP", mvp);
+
+	// draw a quad
+    floorModel->Draw(*shader);
+    //drawQuad();
+}
+
+void drawQuad()
+{
+    static unsigned int quadVAO = 0, quadVBO;
+    if (quadVAO == 0)
+    {
+        float quadVertices[] = {
+            // positions        // texture Coords
+            -1.0f, -1.0f, 0.0f,
+            -1.0f,  1.0f, 0.0f, 
+            1.0f, -1.0f, 0.0f, 
+            1.0f,  1.0f, 0.0f,
+        };
+        // setup plane VAO
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    }
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    glBindVertexArray(0);
+}
+
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (isPaused)
+        return;
+
+    // movement commands
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+
+}
+
+void cursor_input_callback(GLFWwindow* window, double posX, double posY) {
+
+    // camera rotation
+    static bool firstMouse = true;
+    if (firstMouse)
+    {
+        lastX = (float)posX;
+        lastY = (float)posY;
+        firstMouse = false;
+    }
+
+    float xoffset = (float)posX - lastX;
+    float yoffset = lastY - (float)posY; // reversed since y-coordinates go from bottom to top
+
+    lastX = (float)posX;
+    lastY = (float)posY;
+
+    if (isPaused)
+        return;
+
+    // we use the handy camera class from LearnOpenGL to handle our camera
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+
+void key_input_callback(GLFWwindow* window, int button, int other, int action, int mods) {
+    // controls pause mode
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        isPaused = !isPaused;
+        glfwSetInputMode(window, GLFW_CURSOR, isPaused ? GLFW_CURSOR_NORMAL : GLFW_CURSOR_DISABLED);
+    }
+
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll((float)yoffset);
+}
+
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
